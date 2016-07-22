1aa4ac2099588681426f532ee08fe9e7de2ee1ff
Kam Popat <kampopat@gmail.com>
2016-07-22 21:05:03 +0100

    Initial project set up

---
+++ ModRank/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ModRank
+//
+//  Created by Kam Popat on 22/07/2016.
+//  Copyright © 2016 Kam Popat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
